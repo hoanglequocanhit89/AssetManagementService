cb43d1671d3f9d93a8b6b030f8eb9c23a06b405e
hoanglequocanhit89 <anh.hoanglequocit89@gmail.com>
2025-05-22 11:33:09 +0700

    fix: disable user api by taskid 294
    Update application.properties
    
    Add or update the Azure App Service build and deployment workflow config
    
    development: Ignore UT
    
    delvelopment: Add allowed origin for API v1
    
    fix: disable user api by taskid 294

--- src/main/java/com/rookie/asset_management/constant/AllowedOrigins.java
+++ src/main/java/com/rookie/asset_management/constant/AllowedOrigins.java
@@ -3,7 +3,9 @@ package com.rookie.asset_management.constant;
 public final class AllowedOrigins {
 
   public static final String[] ALLOWED_ORIGINS = {
-    "http://localhost:3000", "http://localhost:5173",
+    "http://localhost:3000",
+    "http://localhost:5173",
+    "https://proud-smoke-06841df00.6.azurestaticapps.net",
   };
 
   private AllowedOrigins() {
