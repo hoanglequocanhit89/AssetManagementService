3ddcce905a4bb4c6e84be0561ef890a06aba2b7d
Le Hai Duy <lehaiduy2003@gmail.com>
2025-05-27 10:52:17 +0700

    feat: configure audit trail and logging by taskid 361

--- src/main/java/com/rookie/asset_management/AssetManagementApplication.java
+++ src/main/java/com/rookie/asset_management/AssetManagementApplication.java
@@ -2,8 +2,12 @@ package com.rookie.asset_management;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
+@EnableAspectJAutoProxy
 public class AssetManagementApplication {
 
   public static void main(String[] args) {
