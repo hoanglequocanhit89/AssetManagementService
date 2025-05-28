f4cd7f4bd9b366c602c57867d795527b7f88c105
Vu Tran <vutt1808@gmail.com>
2025-05-28 10:52:13 +0700

    update: add check firstLogin for user

--- src/main/java/com/rookie/asset_management/service/impl/CustomUserDetailsServiceImpl.java
+++ src/main/java/com/rookie/asset_management/service/impl/CustomUserDetailsServiceImpl.java
@@ -1,18 +1,14 @@
 package com.rookie.asset_management.service.impl;
 
 import com.rookie.asset_management.entity.User;
+import com.rookie.asset_management.entity.UserDetailModel;
 import com.rookie.asset_management.exception.AppException;
 import com.rookie.asset_management.repository.UserRepository;
 import com.rookie.asset_management.service.CustomUserDetailsService;
-import java.util.Set;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import org.springframework.http.HttpStatus;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -30,14 +26,8 @@ public class CustomUserDetailsServiceImpl implements CustomUserDetailsService, U
     User user =
         userRepository
             .findByUsername(username)
-            .orElseThrow(() -> new AppException(HttpStatus.UNAUTHORIZED, username + " not found."));
+            .orElseThrow(() -> new AppException(HttpStatus.NOT_FOUND, username + " not found."));
 
-    Set<GrantedAuthority> authorities =
-        Stream.of(user.getRole())
-            .map((role) -> new SimpleGrantedAuthority(role.getName()))
-            .collect(Collectors.toSet());
-
-    return new org.springframework.security.core.userdetails.User(
-        user.getUsername(), user.getPassword(), authorities);
+    return new UserDetailModel(user);
   }
 }
