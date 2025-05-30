47f55b9b618307008c86a82f36ffeeb41a46f40f
NguyenNgan03 <simple.270123@gmail.com>
2025-05-30 12:53:27 +0700

    add validation for changing the password

--- src/main/java/com/rookie/asset_management/dto/request/authentication/FirstLoginChangePasswordRequestDTO.java
+++ src/main/java/com/rookie/asset_management/dto/request/authentication/FirstLoginChangePasswordRequestDTO.java
@@ -1,5 +1,8 @@
 package com.rookie.asset_management.dto.request.authentication;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
 
@@ -9,5 +12,11 @@ import lombok.experimental.FieldDefaults;
 @FieldDefaults(level = AccessLevel.PRIVATE)
 @Builder
 public class FirstLoginChangePasswordRequestDTO {
+  @NotBlank(message = "Password cannot be empty")
+  @Size(min = 8, max = 128, message = "Password must be between 8 and 128 characters")
+  @Pattern(
+      regexp = "^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d)(?=.*[@#$%^&+=!]).{8,128}$",
+      message =
+          "Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character")
   String newPassword;
 }
