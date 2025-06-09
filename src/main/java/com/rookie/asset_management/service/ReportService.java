4b66542134be9eeb058bdc218a183eea4381d4df
Le Hai Duy <lehaiduy2003@gmail.com>
2025-06-09 13:52:39 +0700

    feat: pagination api for report by taskid 771

--- src/main/java/com/rookie/asset_management/service/ReportService.java
+++ src/main/java/com/rookie/asset_management/service/ReportService.java
@@ -1,5 +1,6 @@
 package com.rookie.asset_management.service;
 
+import com.rookie.asset_management.dto.response.PagingDtoResponse;
 import com.rookie.asset_management.dto.response.report.ReportDtoResponse;
 import java.util.List;
 
@@ -11,4 +12,16 @@ public interface ReportService {
    * @return a list of reports
    */
   List<ReportDtoResponse> getAllReports();
+
+  /**
+   * Retrieves a paginated list of reports with optional sorting and search functionality.
+   *
+   * @param page page number to retrieve (0-based index)
+   * @param size number of reports per page
+   * @param sortBy the field to sort by (e.g., "total", "assigned")
+   * @param sortDir the direction of sorting ("asc" for ascending, "desc" for descending)
+   * @return a paginated response containing a list of reports
+   */
+  PagingDtoResponse<ReportDtoResponse> getAllReports(
+      int page, int size, String sortBy, String sortDir);
 }
